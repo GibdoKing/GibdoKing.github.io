40ea838b38b863888d78eb1148dd492b82cfa96e
GibdoKing <140855677+GibdoKing@users.noreply.github.com>
2023-07-28 20:22:59 -0400

    Update expcheck.js

--- expcheck.js
+++ expcheck.js
@@ -1,9 +1,9 @@
 const expold = false;
-if (expold === true) {
-  alert('The latest experimental build is older than the latest stable build. Using latest stable build ...');
-  var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/main.js",document.head.appendChild(runscript),runscript.remove();
-}
-
-else {
-  var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/exp.js",document.head.appendChild(runscript),runscript.remove();
+switch(expold) {
+  case true:
+    alert('The latest experimental build is older than the latest stable build. Using latest stable build ...');
+    var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/main.js",document.head.appendChild(runscript),runscript.remove();
+    break;
+  case false:
+    var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/exp.js",document.head.appendChild(runscript),runscript.remove();
 }
