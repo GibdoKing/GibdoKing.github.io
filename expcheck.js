a485a14437e4c44307b57a91c92f5cca874a7010
GibdoKing <140855677+GibdoKing@users.noreply.github.com>
2023-07-28 20:26:45 -0400

    added comments

--- expcheck.js
+++ expcheck.js
@@ -1,9 +1,9 @@
 const expold = false;
 switch(expold) {
-  case true:
+  case true: // redirects to use latest stable build if exp build is older
     alert('The latest experimental build is older than the latest stable build. Using latest stable build ...');
     var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/main.js",document.head.appendChild(runscript),runscript.remove();
     break;
-  case false:
+  case false: // uses latest exp build
     var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/exp.js",document.head.appendChild(runscript),runscript.remove();
 }
