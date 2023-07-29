560614604fb9de09d84eee5d6dece341140eaec9
GibdoKing <140855677+GibdoKing@users.noreply.github.com>
2023-07-28 20:15:21 -0400

    Create exp.js

---
+++ exp.js
@@ -0,0 +1,2 @@
+alert('There is currently no experimental build at the moment. Using latest stable build ...');
+var runscript=document.createElement("script");runscript.src="https://gibdoking.github.io/main.js",document.head.appendChild(runscript),runscript.remove();
